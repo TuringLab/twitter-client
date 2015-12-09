c968d8058b065c3ebe119f3495e171c56b3ae7d7
Henry Miskin <henry.miskin@talktalk.net>
2015-12-09 17:49:33 +0000

    Added custom style and default data

--- client/script.js
+++ client/script.js
@@ -1,14 +1,40 @@
 
+// Update with new data from server
 function update(query) {
   var url = 'https://turing-twitter.herokuapp.com/' + query;
 
+  // Create temp data
+  url = '/data.json';
+
   $.get(url,function(data){
-    var string = JSON.stringify(data,null,2)
-    $('#results').html(string).show();
+
+    createList(data.tweets);
+    // showRaw(data);
+
   })
+}
+
+function showRaw(data){
+  var string = JSON.stringify(data,null,2)
+  $('#results').html(string).show();
+}
+
+// Create a list of tweets
+function createList(tweets) {
+  var list = $('#list');
+  for (var i in tweets){
+    addLine(list,tweets[i].text);
+  }
+  list.show();
+}
+
+function addLine(list, html) {
+  var line = $(document.createElement('li'));
+  line.html(html);
+  list.append(line);
 }
 
 // When document loaded update data
 $(function(){
-    update();
+    update('turinglab');
 });
