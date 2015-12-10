ffc8eab60c41d595acc7515e741c2a9315551041
Henry Miskin <henry.miskin@talktalk.net>
2015-12-10 14:41:02 +0000

    Created turing bar and bubble interfaces

--- client/turing-chart.js
+++ client/turing-chart.js
@@ -1,10 +1,13 @@
 
-function TuringChart(identifier,axis,data){
+function TuringChart(handle,type,data){
 
-    this.element = $(identifier);
-    this.chart = this.element.highcharts({
+    this.type = type;
+    data = data || [];
+    this.element = $(handle);
+
+    this.options = {
         chart: {
-            type: 'bar'
+            type: this.type
         },
         title: {
             text: this.element.attr('name')
@@ -14,47 +17,82 @@ function TuringChart(identifier,axis,data){
         },
         xAxis: {
             title: {
-                text: axis.y
+                text: null
             }
         },
         yAxis: {
             title: {
-                text: axis.x
-            },
-            // labels: {
-            //     formatter: function () {
-            //         return Math.ceil(100*this.value) + '%';
-            //     }
-            // }
-        },
-
-        plotOptions: {
-            series: {
-                stacking: 'normal'
+                text: null
             }
         },
-
-        // tooltip: {
-        //     formatter: function (test) {
-        //         console.log(test);
-        //         return '<b>' + this.series.name + ', tweet number ' + this.point.category + '</b><br/>' +
-        //             'Content: ' + data.tweets[this.point.index].text;
-        //     }
-        // },
-
         series: [{
-            // name: this.element.attr('name'),
             data: data
-        }]
-    });
+        }],
+        tooltip: {
+            formatter: function(){
+                var text = this.point.text || this.point.name || '';
+                return '<p>' + text + '</p>';
+            }
+        }
+    }
 
-    this.log = function() {
-        console.log(this);
-    } 
+    this.draw = function(){
+        this.chart = this.element.highcharts(this.options);   
+    }
+
+    this.data = function(data){
+        this.options.series[0].data = data;
+        this.draw();
+    };
+
+    this.axis = function(axis){
+        this.options.xAxis.title.text = axis.x;
+        this.options.yAxis.title.text = axis.y;
+        this.draw();
+    }
+
+    this.labels = function(formatter){
+        this.options.yAxis.labels = {
+            formatter: formatter
+        };
+        this.draw();
+    }
+
+    this.tooltip = function(formatter){
+        this.options.tooltip = {
+            formatter: formatter
+        };
+        this.draw();
+    }
+
+    this.draw();
 
 };
 
+function TuringBar(handle,data){
+    var type = 'bar';
+    TuringChart.call(this,handle,type,data);
+}
+
+function TuringBubble(handle,data){
+    var type = 'bubble';
+    TuringChart.call(this,handle,type,data);
+}
+
 $(function(){
-    var chart = new TuringChart('#bar',{x:'Percentage',y:'Product'},[{name:'ballballs',y:-2},{name:'Christmas',y:20}]);
-    chart.log()
+    var chart = new TuringBubble('#bubble');
+
+    chart.data([{name:'ballballs',y:-2},{name:'Christmas',y:20}]);
+    
+    // chart.axis({x:'Percentage',y:'Product'});
+
+    // chart.labels(function() {
+    //     return Math.ceil(100*this.value) + '%';
+    // })
+
+    // chart.tooltip(function() {
+    //     return '<b>' + this.series.name + ', tweet number ' + this.point.category + '</b><br/>' +
+    //         'Content: ' + this.tweets[this.point.index].text;
+    // });
+
 });
