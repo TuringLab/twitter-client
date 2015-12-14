1af9404534a1255456b1cbfa55b09339a3c0e22a
Henry Miskin <henry.miskin@talktalk.net>
2015-12-14 11:00:31 +0000

    Changed functionality to switch between charts

--- client/script.js
+++ client/script.js
@@ -2,30 +2,20 @@
 // Update with new data from server
 function update() {
 
+  // Obtain query string
   var query = $('#query').val();
   var url = 'https://turing-twitter.herokuapp.com/' + query;
 
-  // var url = 'data.json';
-  var display = $('#display').val();
-  console.log(display);
+  // Get data and update display
+  $.get(url,display);
 
-  $.get(url,function(response){
-    if (display == "list") {
-      showList(response);
-    } else if (display == "bar") {
-      showBar(response);
-    } else if (display == "bubble"){
-      showBubble(response);
-    } else {
-      alert('No display selected');
-    }
-  })
 }
 
-// Create a list of tweets
-function showList(response) {
+// Update the display
+function display(response) {
 
-  var list = new TuringList('#results',response.tweets);
+  // Create a list of tweets
+  var list = new TuringList('#list',response.tweets);
   list.formatter(function(tweet){
     var text = '<span">' + tweet.text + '</span>'
     var user = '<strong style="float: right;">' + tweet.username + '</strong>'
@@ -34,58 +24,55 @@ function showList(response) {
     }
     return '<div class="positive">' + text + user + '</div>'
   });
-}
 
+  // Sort tweets by polarity
+  response.tweets.sort(function(a, b){
+    return a.polarity - b.polarity;
+  })
 
-function showBar(response) {
+  var bubbleData = [];
+  var barData = [];
 
-  var tweet;
-  var tweets = response.tweets.sort((tweetA,tweetB) => tweetA.polarity - tweetB.polarity);
-  var data = [];
+  // Loop through all tweets
+  for (var i in response.tweets){
+    var tweet = response.tweets[i];
+    var date = new Date(tweet.createdAt);
 
-  for (var i in tweets){
-    tweet = tweets[i];
-
+    //Ignore neutral tweets
     if (tweet.polarity == 0) continue;
 
-    data.push({
+    // Ignore retweets
+    if (tweet.text.contains('RT')) continue;
+
+    // Add items to bar data
+    barData.push({
       text: tweet.text,
-      y: tweet.polarity,
-    });
+      y: tweet.polarity
+    })
+
+    // Add items to bubble data
+    bubbleData.push({
+      name: tweet.text,
+      x: tweet.polarity, 
+      y: date,
+      z: tweet.text.length
+    })
   }
 
-  var bar = new TuringBar('#results',data);
-  bar.axis({y:'Polarity'});
+  // Create a bar chart
+  var bar = new TuringBar('#bar',barData);
+  bar.axis({y:'Polarity'})
   bar.yLabels(function(){
     return `${Math.floor(this.value * 100)}%`
   })
 
-}
-
-function showBubble(response) {
-
-  var tweet;
-  var date;
-  var data = [];
-
-  for(var i in response.tweets){ 
-    tweet = response.tweets[i];
-    date = new Date(tweet.createdAt)
-
-    data.push({
-      text: tweet.text,
-      x: tweet.polarity,
-      y: new Date(tweet.createdAt),
-      z: tweet.text.length,
-    });
-
-  };
-
-  var bubble = new TuringBubble('#results',data);
-  bubble.axis({x:'Polarity',y:'Time'});
+  // Create a bubble chart
+  var bubble = new TuringBubble('#bubble',bubbleData);
+  bubble.axis({x:'Polarity',y:'Time'})
   bubble.xLabels(function(){
     return `${Math.floor(this.value * 100)}%`
   })
+
 }
 
 function select() {
@@ -95,22 +82,23 @@ function select() {
     $('#list').hide();
     $('#bubble').show();
     $('#bar').hide();
+    $('#help').hide();
   } else if (display == "bar") {
     $('#list').hide();
     $('#bubble').hide();
     $('#bar').show();
-  } else if (display == "list")
+    $('#help').hide();
+  } else {
     $('#list').show();
     $('#bubble').hide();
     $('#bar').hide();
-  } else {
-    $('#list').hide();
-    $('#bubble').hide();
-    $('#bar').hide();
+    $('#help').hide();
   }
 }
 
 // When document loaded update data
 $(function(){
+    $('#display').change(select);
     update();
+    select();
 });
